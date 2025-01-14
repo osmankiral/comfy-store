d268d9810476ece4b5c43306ec4a4556532e951c
osmankiral <osman.kiral96@gmail.com>
2025-01-14 23:55:15 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">App</h1>
+    <h1 className="text-3xl font-bold underline">Hello World</h1>
   )
 }
 export default App
