4278ce24efad31dddc8c1f324da339e6365dae87
osmankiral <osman.kiral96@gmail.com>
2025-01-18 23:47:19 +0300

    added navbar

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,15 +1,15 @@
-import { Outlet } from "react-router-dom"
-
-
+import { Outlet } from "react-router-dom";
+import { Header, Navbar } from "../components";
 
 const HomeLayout = () => {
   return (
-    < >
-    <nav>
-      <span className="text-4xl text-primary">Comfy</span>
-    </nav>
-    <Outlet/>
+    <>
+      <Header/>
+      <Navbar/>
+      <section className="align-element py-20">
+        <Outlet />
+      </section>
     </>
-  )
-}
-export default HomeLayout
+  );
+};
+export default HomeLayout;
