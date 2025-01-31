eb84d5878dec4ef3d8759e28e1f39ab21eafcbc7
osmankiral <osman.kiral96@gmail.com>
2025-01-31 22:11:49 +0300

    ...

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,9 +1,12 @@
 import { Form, Link, useLoaderData } from "react-router-dom";
 import FormInput from "./FormInput";
 import FormSelect from "./FormSelect";
-
+import FormRange from "./FormRange";
+import FormCheckbox from "./FormCheckbox";
+//filters
 const Filters = () => {
-  const { meta } = useLoaderData();
+  const { meta, params } = useLoaderData();
+  const { search, company, category, shipping, order, price } = params;
   return (
     <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
       <FormInput
@@ -11,33 +14,36 @@ const Filters = () => {
         label="search product"
         name="search"
         size="input-sm"
+        defaultValue={search}
       />
-
-      <FormSelect
-        label="select company"
-        name="company"
-        list={meta.companies}
-        size="select-sm"
-      />
-
       <FormSelect
         label="select category"
         name="category"
         list={meta.categories}
         size="select-sm"
+        defaultValue={category}
       />
-
+      <FormSelect
+        label="select company"
+        name="company"
+        list={meta.companies}
+        size="select-sm"
+        defaultValue={company}
+      />
+     
       <FormSelect
         label="sort by"
         name="order"
-        list={['a-z', 'z-a', 'high', 'low']}
+        list={["a-z", "z-a", "high", "low"]}
         size="select-sm"
+        defaultValue={order}
       />
-
-      <button className="btn btn-primary btn-sm" type="submit">
+      <FormRange name="price" label="select price" size="range-sm"  price={price}/>
+      <FormCheckbox label="free shipping" name="shipping" size="checkbox-sm"  defaultValue={shipping}/>
+      <button className="btn btn-primary btn-sm capitalize" type="submit">
         search
       </button>
-      <Link to="/products" className="btn btn-accent btn-sm">
+      <Link to="/products" className="btn btn-accent btn-sm capitalize">
         reset
       </Link>
     </Form>
